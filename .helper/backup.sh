030a2de75892e9eaf7838acde11a72b6eef502dd
8ear <10329648+8ear@users.noreply.github.com>
2020-11-25 22:47:27 +0100

    Fix backup

--- .helper/backup.sh
+++ .helper/backup.sh
@@ -20,7 +20,7 @@ backup_minemeld(){
     echo "stop minemeld docker..."
     docker stop minemeld
     echo "backup minemeld config..."
-    $DOCKER_MINEMELD_CMD tar -cvzf $BACKUP_FILE /opt/minemeld/local/config/ /opt/minemeld/local/prototypes/
+    $DOCKER_MINEMELD_CMD "tar -cvzf $BACKUP_FILE /opt/minemeld/local/config/ /opt/minemeld/local/prototypes/"
     echo "start minemeld docker..."
     docker start minemeld
 }
@@ -41,18 +41,18 @@ backup_cassandra(){
     BACKUP_FILE="/backup/$0_$TIMESTAMP"
     CONTAINER_NAME="thehive-cassandra"
 	docker exec -ti "$CONTAINER_NAME" nodetool cleanup thehive
-	docker exec -ti "$CONTAINER_NAME" nodetool snapshot thehive -t ${$TIMESTAMP}
-	docker exec -ti "$CONTAINER_NAME" tar cjf ${BACKUP_FILE}.tbz /var/lib/cassandra/data/thehive/*/snapshots/${$TIMESTAMP}/
-    docker copy -a "$CONTAINER_NAME":${BACKUP_FILE}.tbz $BASE/BACKUP/cassandra/
-	docker exec -ti "$CONTAINER_NAME" nodetool -h localhost -p 7199 clearsnapshot -t ${TIMESTAMP}
+	docker exec -ti "$CONTAINER_NAME" nodetool snapshot thehive -t "${TIMESTAMP}"
+	docker exec -ti "$CONTAINER_NAME" "tar cjf ${BACKUP_FILE}.tbz /var/lib/cassandra/data/thehive/*/snapshots/${TIMESTAMP}/"
+    docker copy -a "$CONTAINER_NAME:${BACKUP_FILE}.tbz" "$BASE/BACKUP/cassandra/"
+	docker exec -ti "$CONTAINER_NAME" "nodetool -h localhost -p 7199 clearsnapshot -t ${TIMESTAMP}"
 }
 
 restore_cassandra(){
-    ls -la $BASE/BACKUP/cassandra/
+    ls -la "$BASE/BACKUP/cassandra/"
     input -e "Which backupfile?" BACKUP_FILE
-    docker cp -a $BASE/BACKUP/cassandra/$BACKUP_FILE.tbz  cassandra:${BACKUP_FILE}.tbz
+    docker cp -a "$BASE/BACKUP/cassandra/$BACKUP_FILE.tbz"  "cassandra:${BACKUP_FILE}.tbz"
     tar jxf /PATH/TO/backup.tbz -C /
-	docker exec -ti cassandra cd /var/lib/cassandra/data/thehive; for I in `ls /var/lib/cassandra/data/thehive`  ; do cp /var/lib/cassandra/data/thehive/$I/snapshots/backup_name/* /var/lib/cassandra/data/thehive/$I/ ; done
+	docker exec -ti cassandra "cd /var/lib/cassandra/data/thehive; for I in $(ls /var/lib/cassandra/data/thehive)  ; do cp /var/lib/cassandra/data/thehive/$I/snapshots/backup_name/* /var/lib/cassandra/data/thehive/$I/ ; done"
 	docker exec -ti cassandra chown -R cassandra:cassandra /var/lib/cassandra/data/thehive
 	docker restart cassandra
 }
